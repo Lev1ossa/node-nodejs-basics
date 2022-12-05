7858101e2a6382e6e3d305ace24fa3fb1b35928b
Lev1ossa <ikehhui@gmail.com>
2022-12-05 18:25:48 +0300

    done

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,23 @@
+import { spawn } from 'node:child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childProcess = spawn('node', ['./src/cp/files/script.js', ...args]);
+
+  process.stdin.pipe(childProcess.stdin);
+
+  childProcess.stdout.on('data', (data) => {
+    console.log(`Received from child process: ${data}`);
+  });
+
+  childProcess.on('error', (error) => {
+    console.error(error);
+  });
+
+  childProcess.on('close', (code) => {
+    console.log(`Child process exited with code ${code}`);
+  });
+
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['someArgument1', 'someArgument2', 'someArgument3']);
